fea0600c1f8a70a69b7e2f143733bd73e2af0d22
elmatimene <ma.menesesu@duocuc.cl>
2025-06-15 21:24:42 -0400

    empese a utilisar la base de dato
    solo cree las tablas que vamos a ocupar asta ahora

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import reserva
 
 # Register your models here.
+
+admin.site.register(reserva)
--- app/models.py
+++ app/models.py
@@ -2,3 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+class reserva(models.Model):
+    estados= [
+        ('adopatado', 'adopcion'),
+        ('foster', 'temporal'),
+        ('transfer', 'traslado'),
+        ('medical', 'medico'),
+    ]
+
+    motivos =[
+        ('peluqueria'),
+        ('vacuna'),
+        ('atencion medica'),
+    ]
+
+    nombre_dueño = models.CharField(max_length=100)
+    direccion = models.TextField()
+    nombre_mascota = models.CharField(max_length=100)
+    raza = models.CharField(max_length=100, blank=True)
+    fecha_reserva = models.DateField()
+    estado = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.nombre_mascota} - {self.fecha_reserva}"
