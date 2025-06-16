95a934f2b75720e365196397dec0611ff22d895e
elmatimene <ma.menesesu@duocuc.cl>
2025-06-15 22:57:47 -0400

    nosae pero arregle un poco los datos del formulario
    sipo

--- app/models.py
+++ app/models.py
@@ -11,17 +11,19 @@ class reserva(models.Model):
     ]
 
     motivos =[
-        ('peluqueria'),
-        ('vacuna'),
-        ('atencion medica'),
+        ('peluqueria', 'peluqueria'),
+        ('vacuna', 'vacuna'),
+        ('atencion medica', 'atencion medica'),
     ]
 
     nombre_dueño = models.CharField(max_length=100)
+    correo = models.EmailField(blank=True, null=True)
     direccion = models.TextField()
     nombre_mascota = models.CharField(max_length=100)
     raza = models.CharField(max_length=100, blank=True)
     fecha_reserva = models.DateField()
     estado = models.CharField(max_length=50)
+    motivo = models.CharField(max_length=50, choices=motivos, null=True)
     
     def __str__(self):
         return f"{self.nombre_mascota} - {self.fecha_reserva}"
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from .models import reserva
 
 # Create your views here.
 
@@ -7,7 +9,30 @@ def home(request):
 
 
 def agendar(request):
-    return render(render, 'app/agendar.html')
+    if request.method == 'POST':
+        nombre_dueño = request.POST.get('dueno')
+        correo = request.POST.get('correo')
+        direccion = request.POST.get('direccion')
+        nombre_mascota = request.POST.get('animalname')
+        raza = request.POST.get('breedname')
+        fecha_reserva = request.POST.get('fecha')
+        estado = request.POST.get('estado')
+        motivo = request.POST.get('motivo')
+
+        reserva.objects.create(
+            nombre_dueño=nombre_dueño,
+            correo=correo,
+            direccion=direccion,
+            nombre_mascota=nombre_mascota,
+            raza=raza,
+            fecha_reserva=fecha_reserva,
+            estado=estado,
+            motivo=motivo
+        )
+
+        messages.success(request, 'reserva ehcha con exito')
+        return redirect ('home')
+    return render(request, 'app/agendar.html')
 
 def nav(request):
     return render(render, 'app/nav_foot/nav')
